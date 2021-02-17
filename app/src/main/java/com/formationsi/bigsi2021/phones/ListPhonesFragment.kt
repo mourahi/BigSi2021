8894250777a2f4d53791f360818b66cac49c1d51
Adil MOURAHI <adil.mourahi@gmail.com>
2021-02-17 22:49:06 +0100

    recycleview

--- app/src/main/java/com/formationsi/bigsi2021/phones/ListPhonesFragment.kt
+++ app/src/main/java/com/formationsi/bigsi2021/phones/ListPhonesFragment.kt
@@ -2,11 +2,17 @@ package com.formationsi.bigsi2021.phones
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.formationsi.bigsi2021.R
+import com.formationsi.bigsi2021.adapter.RecycleAdapterPhones
 
 class ListPhonesFragment : Fragment() {
 
@@ -26,7 +32,19 @@ class ListPhonesFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ListPhonesViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.getData().observe(this, {
+            Log.d("adil","it = $it")
+        })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recycle = view.findViewById<RecyclerView>(R.id.recycle_phones)
+        recycle.layoutManager = LinearLayoutManager(view.context)
+        val mylist = listOf(Phone("adil",40), Phone("saida", 30))
+        val adapter = RecycleAdapterPhones(mylist)
+        recycle.adapter = adapter
+
     }
 
 }
--- app/src/main/java/com/formationsi/bigsi2021/phones/ListPhonesViewModel.kt
+++ app/src/main/java/com/formationsi/bigsi2021/phones/ListPhonesViewModel.kt
@@ -1,7 +1,16 @@
 package com.formationsi.bigsi2021.phones
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ListPhonesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val data = MutableLiveData<Phone>()
+    fun getData():LiveData<Phone>{
+         data.value = Phone("adil", 22)
+        return data
+    }
+
 }
+
+data class Phone(val name:String, val age: Int)
